8f2e40ec2caa4f79617e8011e776d4c31c93377f
Zach Schwartz <zachatorcodes@gmail.com>
2022-10-28 17:43:25 -0600

    Completed basic functionallity for adding and removing tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleNewTask(e.target["new-task-description"].value);
+    form.reset();
+  });
 });
+
+function handleNewTask(task) {
+  const li = document.createElement("li");
+  const button = document.createElement("button");
+  button.addEventListener("click", handleRemoveTask);
+  button.textContent = "X";
+  li.textContent = `${task} `;
+  li.appendChild(button);
+  document.querySelector("#tasks").appendChild(li);
+}
+
+function handleRemoveTask(event) {
+  event.target.parentNode.remove();
+}
