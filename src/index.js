e03cfe31641827ecf4a7b60a487d71e0fbed63b4
Zach Schwartz <zachatorcodes@gmail.com>
2022-10-31 13:14:06 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,26 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    handleNewTask(e.target["new-task-description"].value);
+    handleNewTask((e.target["new-task-description"].value), (e.target["dropdown"].value));
     form.reset();
   });
 });
 
-function handleNewTask(task) {
+function handleNewTask(task, priority) {
   const li = document.createElement("li");
   const button = document.createElement("button");
   button.addEventListener("click", handleRemoveTask);
-  button.textContent = "X";
+  button.textContent = "❌";
   li.textContent = `${task} `;
+  if (priority === "high-priority") {
+    li.style.color = "red";
+  }
+  else if (priority === "medium-priority") {
+    li.style.color = "yellow";
+  }
+  else if (priority === "low-priority") {
+    li.style.color = "green";
+  }
   li.appendChild(button);
   document.querySelector("#tasks").appendChild(li);
 }
